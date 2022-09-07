890779c010df8d45be923c93403bf8abdcf176c8
Mateusz-Gonera <mateuszg1992@wp.pl>
2022-09-07 15:18:34 +0200

    dodanie plików JS

--- src/js/fetchResponse.js
+++ src/js/fetchResponse.js
@@ -1,11 +1,32 @@
 const API_KEY = '18cc3ec683925dedb66d3e6092890eaa';
 
-const fetchResponse = async (search, pagenr) => {
-  const response = await fetch(
-    ``
+const fetchResponseTrend = async pagenr => {
+  const responseTrend = await fetch(
+    `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}&page=${pagenr}`
   );
-  const movies = await response.json();
-  return movies;
+  if (!responseTrend.ok) throw new Error();
+  const moviesTrend = await responseTrend.json();
+  return moviesTrend;
 };
 
-export { fetchResponse };
+const fetchResponseSearch = async (search, pagenr) => {
+  const responseSearch = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=pl-PL&query=${search}&page=${pagenr}&include_adult=false`
+  );
+  if (!responseSearch.ok) throw new Error();
+  const moviesSearch = await responseSearch.json();
+  return moviesSearch;
+};
+
+const fetchResponseDetails = async movieID => {
+  const responseDetails = await fetch(
+    `https://api.themoviedb.org/3/movie/${movieID}?api_key=${API_KEY}&language=pl-PL`
+  );
+  if (!responseDetails.ok) throw new Error();
+  const moviesDetails = await responseDetails.json();
+  return moviesDetails;
+};
+
+
+
+export { fetchResponseTrend, fetchResponseSearch, fetchResponseDetails };
--- src/js/myLibrarySite.js
+++ src/js/myLibrarySite.js
@@ -2,3 +2,5 @@ import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
 import { fetchResponseTrend, fetchResponseSearch, fetchResponseDetails } from './js/fetchResponse';
 
+
+
