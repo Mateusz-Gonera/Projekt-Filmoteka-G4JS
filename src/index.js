bc5169b7abb9c5729c8fee9c8507cfe858336f02
Mateusz-Gonera <mateuszg1992@wp.pl>
2022-09-07 16:35:31 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
 import { fetchResponseTrend, fetchResponseSearch, fetchResponseDetails } from './js/fetchResponse';
-import { s } from './js/mainPagination';
