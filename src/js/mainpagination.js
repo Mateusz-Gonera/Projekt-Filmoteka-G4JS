003ae3d29709be9ac153f1bdcc7e348ea07c2a76
Mateusz-Gonera <mateuszg1992@wp.pl>
2022-09-07 15:34:16 +0200

    pliki JS gotowe

--- src/js/mainpagination.js
+++ src/js/mainpagination.js
@@ -1,3 +1,7 @@
 import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
 import { fetchResponseTrend, fetchResponseSearch, fetchResponseDetails } from './js/fetchResponse';
+
+const s = 1;
+
+export { s };
