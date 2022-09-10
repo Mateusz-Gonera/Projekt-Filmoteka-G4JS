bb45e1bace6ce5455452a1a75b774aa91f7d4c6e
Michalinius <michalinius@gmail.com>
2022-09-10 16:40:03 +0200

    brak wyników wyswietla komunikat, dodano .catch

--- src/js/search.js
+++ src/js/search.js
@@ -4,17 +4,25 @@ import { fetchResponseSearch } from "./fetchResponse";
 const query = document.querySelector("#searchbox");
 const searchForm = document.querySelector(".search__form");
 const injectionDiv = document.querySelector(".film-list");
+const info = document.querySelector(".infoPlace");
+
+
 
 searchForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    console.log(fetchResponseSearch(query.value, 1))
+    console.log(fetchResponseSearch(query.value, 1));
     fetchResponseSearch(query.value, 1)
         .then(data => {
             injectionDiv.innerHTML = "";
-            for (let i = 1; i < data.results.length; i++) {
-                injectionDiv.insertAdjacentHTML("beforeend", `<div class="single-film">
+            if (data.results.length == 0) {
+                info.innerHTML = "Search result not successful. Enter the correct movie name and ";
+            }
+            else {
+                info.innerHTML = "";
+                for (let i = 1; i < data.results.length; i++) {
+                    injectionDiv.insertAdjacentHTML("beforeend", `<div class="single-film">
                 <img class="film-image" src="https://image.tmdb.org/t/p/w500${data.results[i].poster_path}"
-            alt="${data.results[i].release_date}  API nie zwraca alternatywnego opisu">
+            alt="movie card">
         <div class="film-info">
             <p class="film-title">${data.results[i].original_title}</p>
             <div class="film-subinfo">
@@ -23,8 +31,15 @@ searchForm.addEventListener("submit", (event) => {
             </div>
         </div>
     </div>`)
+                }
             }
-        });
+        })
+        .catch((error) => {
+            injectionDiv.innerHTML = "";
+            info.innerHTML = "";
+            console.warn(error);
+        })
+
 
 })
 
