7f75f8dc2fab89c32c7f8e8286d24e9a0a1c4f9c
Mateusz-Gonera <mateuszg1992@wp.pl>
2022-09-12 19:19:54 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
 import { fetchResponseTrend, fetchResponseSearch, fetchResponseDetails, getGenreNames } from './js/fetchResponse';
 import { s } from './js/mainpagination';
+
