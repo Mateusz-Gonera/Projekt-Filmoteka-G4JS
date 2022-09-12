50016da970c4911c0c2e11d55fd6974eb5a884f9
Mateusz-Gonera <mateuszg1992@wp.pl>
2022-09-12 12:33:34 +0200

    dodanie gatunków

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
-import { fetchResponseTrend, fetchResponseSearch, fetchResponseDetails } from './js/fetchResponse';
+import { fetchResponseTrend, fetchResponseSearch, fetchResponseDetails, getGenreNames } from './js/fetchResponse';
 import { s } from './js/mainpagination';
