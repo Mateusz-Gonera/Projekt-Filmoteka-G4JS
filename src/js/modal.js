c0981deefa893545772d39a6b333df8bc2661c79
Klaudia Król <100020626+KlaudiaK0205@users.noreply.github.com>
2022-09-13 09:54:50 +0200

    Our Team Btn

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,6 +1,6 @@
-import Notiflix from 'notiflix';
-import debounce from 'lodash.debounce';
-import { fetchResponseTrend, fetchResponseSearch, fetchResponseDetails } from './fetchResponse';
+// import Notiflix from 'notiflix';
+// import debounce from 'lodash.debounce';
+// import { fetchResponseTrend, fetchResponseSearch, fetchResponseDetails } from './fetchResponse';
 
 
 
