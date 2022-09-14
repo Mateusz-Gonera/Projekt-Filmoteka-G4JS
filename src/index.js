81f7f368233025b896aa14c0404eca1460f55823
Dominika Sosnowska <domi.sosnowska@o2.pl>
2022-09-14 19:07:51 +0200

    rozwiązanie konfliktów

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
 import { fetchResponseTrend, fetchResponseSearch, fetchResponseDetails, getGenreNames } from './js/fetchResponse';
-import './js/mainpagination'
+import { s } from './js/mainpagination';
