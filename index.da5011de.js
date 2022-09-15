927bf7c346d6f75ae88cb13d85412cbe88ad14f7
Mateusz-Gonera <99548853+Mateusz-Gonera@users.noreply.github.com>
2022-09-15 11:47:02 +0000

    Deploying to gh-pages from @ Mateusz-Gonera/Projekt-Filmoteka-G4JS@845bece03133d193ef38aeaa30bb6465da7dfeeb 🚀

---
+++ index.da5011de.js
@@ -0,0 +1,2 @@
+window.addEventListener("load",(()=>{const d=document.querySelector(".loader");d.classList.add("loader-hidden"),d.addEventListener("transitionend",(()=>{document.body.removeChild("loader")}))}));
+//# sourceMappingURL=index.da5011de.js.map
