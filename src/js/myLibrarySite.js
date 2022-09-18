d7cd0625d6115b42554483caecfc4006b8935373
PiotrKoczkodaj <piotr.koczkodaj@vp.pl>
2022-09-18 22:30:32 +0200

    update

--- src/js/myLibrarySite.js
+++ src/js/myLibrarySite.js
@@ -2,6 +2,20 @@ import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
 import { fetchResponseTrend, fetchResponseSearch, fetchResponseDetails, getGenreNames } from './fetchResponse';
 
+const filmList = document.querySelector('.film-list');
+const myLibrary = document.querySelector('a[href="/myLibrarySite.html"]');
+const mainContainer = document.querySelector('.main_container');
 
 
-console.log("czesc")
+myLibrary.addEventListener("click", (event) => {
+   
+    event.preventDefault();
+    
+    const clearfilmList = mainContainer.innerHTML = ``;
+
+    mainContainer += clearfilmList;
+
+    
+})
+
+
