c1face36ab42cd03f850f999f1f9c9cefd0ea76b
Dominika Sosnowska <domi.sosnowska@o2.pl>
2022-09-18 16:56:04 +0200

    modal

--- src/js/mainpagination.js
+++ src/js/mainpagination.js
@@ -7,6 +7,9 @@ import {
   getGenreNames,
 } from './fetchResponse';
 
+export { changePage, addFilms, ChangeFilmInfo, scrollup };
+
+//======= PAGINATION / SEARCH =======//
 const pagination = document.querySelector('.pagination__list');
 const form = document.querySelector('.search__form');
 const input = document.querySelector('input');
@@ -18,6 +21,23 @@ const next = document.querySelector('.next');
 prev.style.display = 'none';
 
 let page = 1;
+
+//======= MODAL =======//
+const modalPicture = document.querySelector('.modal__img-picture');
+const modalTitle = document.querySelector('.modal__head');
+const modalRating = document.querySelector('.modal__rating');
+const modalVotes = document.querySelector('.modal__votes-number');
+const modalPopularity = document.querySelector('.modal__popularity-total');
+const modalOriginalTitle = document.querySelector('.modal__title-original');
+const modalGenre = document.querySelector('.modal__genre-description');
+const modalDescription = document.querySelector('.modal__text');
+const modalBtnWatched = document.querySelector('.modal__button-watched');
+const modalBtnQueued = document.querySelector('.modal__button-queue');
+
+let watchedFilmsStorage = [];
+let queuedFilmsStorage = [];
+localStorage.setItem('Wathced films', JSON.stringify(watchedFilmsStorage));
+localStorage.setItem('Queued films', JSON.stringify(queuedFilmsStorage));
 
 function changePage(totalPages, page) {
   let liTag = '';
@@ -42,7 +62,7 @@ function changePage(totalPages, page) {
         liTag += `<li class="dots"><span>...</span></li>`;
       }
     }
-    if (totalPages > 5) {
+    if (totalPages > 6) {
       if (page === totalPages) {
         beforePages = beforePages - 2;
       }
@@ -93,13 +113,23 @@ function changePage(totalPages, page) {
   pagination.innerHTML = liTag;
 }
 
+const ChangeFilmInfo = async film => {
+  modalPicture.src = `https://image.tmdb.org/t/p/w400/${film.poster_path}`;
+  modalTitle.innerHTML = `${film.title}`;
+  modalRating.innerHTML = `${film.vote_average.toFixed(1)}`;
+  modalVotes.innerHTML = `${film.vote_count}`;
+  modalPopularity.innerHTML = `${film.popularity.toFixed(1)}`;
+  modalOriginalTitle.innerHTML = `${film.original_title}`;
+  modalGenre.innerHTML = `${film.genres.map(genre => genre.name).join(', ')}`;
+  modalDescription.innerHTML = `${film.overview}`;
+};
+
 const addFilms = films => {
-  filmList.innerHTML = '';
   const movies = films.results;
   const markup = movies
     .map(
       film =>
-        `<div class="single-film">
+        `<div class="single-film" data-id="${film.id}" data-modal-open>
             <img class="film-image" src="https://image.tmdb.org/t/p/w400/${film.poster_path}"
                 alt="${film.title}">
             <div class="film-info">
@@ -114,8 +144,82 @@ const addFilms = films => {
     .join('');
   filmList.innerHTML = markup;
 
-  const filmGenre = document.querySelectorAll('.film-genre');
+  const filmArray = document.querySelectorAll('.single-film');
+  filmArray.forEach(film => {
+    film.addEventListener('click', async e => {
+      e.preventDefault();
+      const filmId = e.target.dataset.id;
+      console.log(filmId);
+      const filmData = await fetchResponseDetails(filmId);
+      ChangeFilmInfo(filmData);
 
+      modalBtnWatched.addEventListener('click', () => {
+        watchedFilmsStorage
+          .push({
+            id: filmData.id,
+            poster_path: filmData.poster_path,
+            title: filmData.title,
+            release_date: filmData.release_date,
+            vote_average: filmData.vote_average,
+            vote_count: filmData.vote_count,
+            popularity: filmData.popularity,
+            original_title: filmData.original_title,
+            genres: filmData.genres,
+            overview: filmData.overview,
+          });
+        localStorage.setItem(
+          'Wathced films',
+          JSON.stringify(watchedFilmsStorage)
+        );
+      });
+
+      modalBtnQueued.addEventListener('click', () => {
+        queuedFilmsStorage.push({
+          id: filmData.id,
+          poster_path: filmData.poster_path,
+          title: filmData.title,
+          release_date: filmData.release_date,
+          vote_average: filmData.vote_average,
+          vote_count: filmData.vote_count,
+          popularity: filmData.popularity,
+          original_title: filmData.original_title,
+          genres: filmData.genres,
+          overview: filmData.overview,
+        });
+        localStorage.setItem(
+          'Queued films',
+          JSON.stringify(queuedFilmsStorage)
+        );
+      });
+    });
+  });
+
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelectorAll('[data-modal-open]'),
+      closeModalBtn: document.querySelector('[data-modal-close]'),
+      modal: document.querySelector('[data-modal]'),
+    };
+    refs.openModalBtn.forEach(open =>
+      open.addEventListener('click', toggleModal)
+    );
+
+    function toggleModal() {
+      refs.modal.classList.toggle('is-hidden');
+    }
+    if (!refs.modal.classList.contains('is-hidden')) {
+      refs.closeModalBtn.addEventListener('click', () => {
+        refs.modal.classList.add('is-hidden');
+      });
+      document.addEventListener('keydown', e => {
+        if (e.key == 'Escape') {
+          refs.modal.classList.add('is-hidden');
+        }
+      });
+    }
+  })();
+
+  const filmGenre = document.querySelectorAll('.film-genre');
   const addGenres = async () => {
     const genreId = movies.map(film => film.genre_ids);
     for (let i = 0; i <= filmGenre.length; i++) {
@@ -133,6 +237,54 @@ const scrollup = () => {
     behavior: 'smooth',
   });
 };
+
+fetchResponseTrend(page).then(popularMovies => {
+  addFilms(popularMovies);
+  changePage(popularMovies.total_pages, page);
+
+  pagination.addEventListener('click', async event => {
+    if (isNaN(event.target.textContent)) return;
+    else {
+      page = Number(event.target.textContent);
+
+      const nextPage = await fetchResponseTrend(page);
+      scrollup();
+      addFilms(nextPage);
+      changePage(nextPage.total_pages, page);
+
+      if (page == 1) prev.style.display = 'none';
+      else prev.style.display = 'block';
+
+      if (page == nextPage.total_pages) next.style.display = 'none';
+      else next.style.display = 'block';
+    }
+  });
+
+  if (page > 1) {
+    prev.addEventListener('click', async () => {
+      page = page - 1;
+      const nextPage = await fetchResponseTrend(page);
+      scrollup();
+      addFilms(nextPage);
+      changePage(nextPage.total_pages, page);
+      if (page === 1) prev.style.display = 'none';
+      next.style.display = 'block';
+    });
+  }
+
+  next.addEventListener('click', async () => {
+    page = page + 1;
+    const nextPage = await fetchResponseTrend(page);
+    scrollup();
+    addFilms(nextPage);
+    changePage(nextPage.total_pages, page);
+    if (page === nextPage.total_pages) next.style.display = 'none';
+    prev.style.display = 'block';
+  });
+});
+pagination.removeEventListener('submit', {});
+next.removeEventListener('submit', {});
+prev.removeEventListener('submit', {});
 
 form.addEventListener('submit', e => {
   let tipedInput = input.value.trim();
@@ -172,8 +324,8 @@ form.addEventListener('submit', e => {
           }
         });
 
-        prev.addEventListener('click', async () => {
-          if (page > 1) {
+        if (page > 1) {
+          prev.addEventListener('click', async () => {
             page = page - 1;
             const nextPage = await fetchResponseSearch(tipedInput, page);
             scrollup();
@@ -181,8 +333,8 @@ form.addEventListener('submit', e => {
             changePage(nextPage.total_pages, page);
             if (page === 1) prev.style.display = 'none';
             next.style.display = 'block';
-          }
-        });
+          });
+        }
 
         next.addEventListener('click', async () => {
           page = page + 1;
@@ -199,51 +351,3 @@ form.addEventListener('submit', e => {
     console.log(error.message);
   }
 });
-
-fetchResponseTrend(page).then(popularMovies => {
-  addFilms(popularMovies);
-  changePage(popularMovies.total_pages, page);
-
-  pagination.addEventListener('click', async event => {
-    if (isNaN(event.target.textContent)) return;
-    else {
-      page = Number(event.target.textContent);
-
-      const nextPage = await fetchResponseTrend(page);
-      scrollup();
-      addFilms(nextPage);
-      changePage(nextPage.total_pages, page);
-
-      if (page == 1) prev.style.display = 'none';
-      else prev.style.display = 'block';
-
-      if (page == nextPage.total_pages) next.style.display = 'none';
-      else next.style.display = 'block';
-    }
-  });
-
-  prev.addEventListener('click', async () => {
-    if (page > 1) {
-      page = page - 1;
-      const nextPage = await fetchResponseTrend(page);
-      scrollup();
-      addFilms(nextPage);
-      changePage(nextPage.total_pages, page);
-      if (page === 1) prev.style.display = 'none';
-      next.style.display = 'block';
-    }
-  });
-
-  next.addEventListener('click', async () => {
-    page = page + 1;
-    const nextPage = await fetchResponseTrend(page);
-    scrollup();
-    addFilms(nextPage);
-    changePage(nextPage.total_pages, page);
-    if (page === nextPage.total_pages) next.style.display = 'none';
-    prev.style.display = 'block';
-  });
-});
-pagination.removeEventListener('submit', {});
-next.removeEventListener('submit', {});
-prev.removeEventListener('submit', {});
