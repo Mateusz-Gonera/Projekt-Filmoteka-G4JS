dcdc315c66a5073d56f8720dad3fd4cb4e820997
Mateusz-Gonera <mateuszg1992@wp.pl>
2022-09-19 12:14:16 +0200

    Revert "poprawki"
    This reverts commit 83918f9c1b5fbc500c8f746fcfac73b89d8db1cb.

--- src/js/myLibrarySite.js
+++ src/js/myLibrarySite.js
@@ -3,13 +3,19 @@ import debounce from 'lodash.debounce';
 import { fetchResponseTrend, fetchResponseSearch, fetchResponseDetails, getGenreNames } from './fetchResponse';
 
 const filmList = document.querySelector('.film-list');
-// const myLibrary = document.querySelector('a[href="/myLibrarySite.html"]');
-const mainContainer = document.querySelector('.main__container-library');
-const watchedBtn = document.querySelector('.myLibrary__button');
-
-mainContainer.innerHTML = '';
+const myLibrary = document.querySelector('a[href="/myLibrarySite.html"]');
+const mainContainer = document.querySelector('.main_container');
 
 
+myLibrary.addEventListener("click", (event) => {
+   
+    event.preventDefault();
+    
+    const clearfilmList = mainContainer.innerHTML = ``;
 
+    mainContainer += clearfilmList;
+
+    
+})
 
 
