83918f9c1b5fbc500c8f746fcfac73b89d8db1cb
Mateusz-Gonera <mateuszg1992@wp.pl>
2022-09-19 11:28:54 +0200

    poprawki

--- src/js/myLibrarySite.js
+++ src/js/myLibrarySite.js
@@ -3,19 +3,13 @@ import debounce from 'lodash.debounce';
 import { fetchResponseTrend, fetchResponseSearch, fetchResponseDetails, getGenreNames } from './fetchResponse';
 
 const filmList = document.querySelector('.film-list');
-const myLibrary = document.querySelector('a[href="/myLibrarySite.html"]');
-const mainContainer = document.querySelector('.main_container');
+// const myLibrary = document.querySelector('a[href="/myLibrarySite.html"]');
+const mainContainer = document.querySelector('.main__container-library');
+const watchedBtn = document.querySelector('.myLibrary__button');
+
+mainContainer.innerHTML = '';
 
 
-myLibrary.addEventListener("click", (event) => {
-   
-    event.preventDefault();
-    
-    const clearfilmList = mainContainer.innerHTML = ``;
 
-    mainContainer += clearfilmList;
-
-    
-})
 
 
