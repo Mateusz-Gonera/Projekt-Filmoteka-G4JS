6270e5edd0b769067816ec4e380c532142a856a4
Mateusz-Gonera <99548853+Mateusz-Gonera@users.noreply.github.com>
2022-09-26 17:31:36 +0000

    Deploying to gh-pages from @ Mateusz-Gonera/Projekt-Filmoteka-G4JS@5ff84c770d85048d84e0dfefa3b9d5d14637252e 🚀

---
+++ index.0753c56a.js
@@ -0,0 +1,2 @@
+const o=document.querySelector(".scroll_top");window.addEventListener("scroll",(function(){o.classList.toggle("active",window.scrollY>500)})),o.addEventListener("click",(function(){window.scrollTo({top:0,behavior:"smooth"})}));
+//# sourceMappingURL=index.0753c56a.js.map
---
+++ index.b4cf5d39.js
@@ -0,0 +1,2 @@
+!function(){var o=document.querySelector(".scroll_top");window.addEventListener("scroll",(function(){o.classList.toggle("active",window.scrollY>500)})),o.addEventListener("click",(function(){window.scrollTo({top:0,behavior:"smooth"})}))}();
+//# sourceMappingURL=index.b4cf5d39.js.map
